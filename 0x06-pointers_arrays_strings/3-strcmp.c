f4786768a8be83503fa67144df61ba0bda55f633
Ozioma7 <okonisaozioma@gmail.com>
2023-04-01 16:54:20 +0000

    task3

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -4,14 +4,14 @@
  * _strcmp - Function that compares two strings
  * @s1: first number
  * @s2: second number
- * Return: Product
+ * Return: 0
  */
 
 int _strcmp(char *s1, char *s2)
 {
 	int i;
 
-	for (i = 0; s1[i] != '\0' || s2[i] != '\0'; i++)
+	for (i = 0 ; s1[i] != '\0' || s2[i] != '\0' ; i++)
 	{
 		if (s1[i] != s2[i])
 		{
