7d4f39549ffbf1b37d9fa03c53ae7eef0038c3f6
Ozioma7 <okonisaozioma@gmail.com>
2023-04-01 15:48:27 +0000

    task 2

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _strncpy - Function that copies two strings
+ * @dest: One of the strings
+ * @src: The other string
+ * Return: Copied String
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	char dest;
+	char src;
+
+	strcpy(src, dest);
+
+	_putchar(src);
+
+	return (0);
+}
