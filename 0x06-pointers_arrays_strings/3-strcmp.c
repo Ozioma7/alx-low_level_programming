071591af3a1316042e53f5abacba839ee2f1114c
Ozioma7 <okonisaozioma@gmail.com>
2023-04-01 18:38:04 +0000

    task 3

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -9,29 +9,29 @@
 
 int _strcmp(char *s1, char *s2)
 {
-	int a = 0, dif = 0;
+	int a = 0, diff = 0;
 
 	while (1)
 	{
-		if (s1[a] == '\0' && s2[i] == '\0')
+		if (s1[a] == '\0' && s2[a] == '\0')
 			break;
 		else if (s1[a] == '\0')
 		{
-			dif = s2[a];
+			diff = s2[a];
 			break;
 		}
 		else if (s2[a] == '\0')
 		{
-			dif = s1[a];
+			diff = s1[a];
 			break;
 		}
 		else if (s1[a] != s2[a])
 		{
-			dif = s1[a] - s2[a];
+			diff = s1[a] - s2[a];
 			break;
 		}
 		else
 			a++;
 	}
-	return (dif);
+	return (diff);
 }
