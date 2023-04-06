4816303b2515dadd32d7412e69cb5f6a0d9ade6d
Ozioma7 <okonisaozioma@gmail.com>
2023-04-06 16:08:27 -0700

    task 0

---
+++ 0-puts_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**_puts_recursion - function that prints a string, followed by a new line.
+ * @s: string's pointer
+ *
+ *Return: void
+ */
+
+void _puts_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		_putchar('\0');
+		return;
+	}
+	_putchar(*s);
+	_putsd_recusion(s + 1);
+}
