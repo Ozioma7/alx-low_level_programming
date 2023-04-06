412e4a5d48c98bb5d18142517e9cab21a3868012
Ozioma7 <okonisaozioma@gmail.com>
2023-04-06 16:42:50 -0700

    task 2

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,19 @@
+#include "main'h"
+
+/**
+ * _strlen_recursion - function that returns the length of a string
+ *@s: string's pointer
+ *Return: String's lenght
+ */
+
+int _strlen_recursion(char *s)
+{
+	if (*s)
+	{
+		return (1 + _strlen_recursion(s + 1));
+	}
+	else
+	{
+		return (0);
+	}
+}
