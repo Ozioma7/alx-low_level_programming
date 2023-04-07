482e96ca886460b105a63eca911ead3d681d5eb2
Ozioma7 <okonisaozioma@gmail.com>
2023-04-06 18:43:07 -0700

    task five

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,6 +1,28 @@
 #include "main.h"
 
 /**
+ * partialfunction - Checker's function
+ * @numb: Number
+ * @SQRT: Square root of numb
+ * Return: square root of -1
+ */
+
+int partialfunction(int numb, int SQRT)
+{
+	if ((numb * numb) == SQRT)
+	{
+		return (SQRT);
+	}
+	else
+	{
+		if ((numb * numb) > SQRT)
+			return (-1);
+		else
+			return (partialfunction(SQRT, numb + 1));
+	}
+}
+
+/**
  *_sqrt_recursion - function that returns the
  *natural square root of a number
  *@n: The number to find its square root
@@ -13,5 +35,5 @@ int _sqrt_recursion(int n)
 	if (n < 0)
 		return (-1);
 	else
-		return (_sqrt_recursion(n, 0));
+		return (partialfunction(n, 0));
 }
