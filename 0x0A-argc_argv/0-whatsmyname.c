49be7a6ed3f6b8efa819ee9a39ac95e4281903e2
Ozioma7 <okonisaozioma@gmail.com>
2023-04-10 07:05:14 -0700

    task 0

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -8,9 +8,13 @@
  * Return: 0
  */
 
-int main(int argc, char *argv[])
+void main(int argc, char *argv[])
 {
-	printf("%s\n", *argv);
+	int a;
+	for (a = 0; a < argc; a++)
+	{
+		printf("%s\n", argv[a]);
+	}
 
 	return (0);
 }
