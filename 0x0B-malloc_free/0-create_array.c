276cb60ddc8ebf7b3d74834ca1b133947e5a5f59
Ozioma7 <okonisaozioma@gmail.com>
2023-04-19 01:23:33 -0700

    task0

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -16,9 +16,13 @@ char *create_array(unsigned int size, char c)
 	string = malloc(sizeof(char) * size);
 
 	if (size == 0 || string == NULL)
+
+
 		return (NULL);
 
 	for {(a = 0; a < size; a++)
-	       string[a] = c; }
+		string[a] = c;
+	}
+
 	return (string);
 }
