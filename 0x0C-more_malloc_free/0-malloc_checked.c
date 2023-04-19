ca00998a52f40e8b7a611125db5b590478a887ae
Ozioma7 <okonisaozioma@gmail.com>
2023-04-19 08:18:26 -0700

    task 0

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * malloc_checked - function that allocates
+ * memory using malloc
+ * @b: memory
+ * Return: void
+ */
+
+void *malloc_checked(unsigned int b)
+{
+	void *pt;
+
+	pt = *malloc(b);
+
+	if (pt == NULL)
+	{
+		exit(98);
+	}
+	return (pt);
+}
