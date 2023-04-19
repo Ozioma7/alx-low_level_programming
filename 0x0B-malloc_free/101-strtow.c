52b5553f7f2ca4aeb3ea8f427c8540b0429f9ac7
Ozioma7 <okonisaozioma@gmail.com>
2023-04-19 05:46:42 -0700

    task101

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -1,4 +1,4 @@
-#include <stdlid.h>
+#include <stdlib.h>
 #include <stdio.h>
 #include "main.h"
 
@@ -21,7 +21,7 @@ int wordcount(char *stng)
 		else if (flag == 0)
 		{
 			flag = 1;
-			wrd;
+			wrd++;
 		}
 
 	}
