7ca1830a2934aa8f26a295d7dae4aa3f72a9b716
Ozioma7 <okonisaozioma@gmail.com>
2023-04-19 08:50:02 -0700

    task 1

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,52 @@
+#include "main.h"
+#include <stdlib.h>
+
+
+/**
+ * string_nconcat - function that concatenates two strings
+ * @s1: string 1
+ * @s2: string 2
+ * @n: index
+ * Return: Result
+ */
+
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	char *pt;
+	unsigned int sz1 = 0, sz2 = 0, a;
+
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+
+	while (s1[sz1] != '\0')
+	{
+		sz1++;
+	}
+
+	while (s2[sz2] != '\0')
+	{
+		sz2++;
+	}
+
+	if (n > sz2)
+	n = sz2;
+	pt = malloc((z1 + n + 1) * sizeof(char));
+
+	if (pt == NULL)
+		return (0);
+
+	for (a = 0; a < sz1; a++)
+	{
+		pt[a] = s1[a];
+	}
+
+	for (; a < (sz1 + n); a++)
+	{
+		pt[a] = s2[a - sz1];
+	}
+	pt[a] = '\0';
+
+	return (pt);
+}
