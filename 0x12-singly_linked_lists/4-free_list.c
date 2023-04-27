3990b9b7919129e28a184a608c015320d1a08597
Ozioma7 <okonisaozioma@gmail.com>
2023-04-27 19:23:25 +0000

    freeList

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -9,12 +9,12 @@
 
 void free_list(list_t *head)
 {
-	list_t *new;
+	list_t *newNode;
 
-	while ((new = head) != NULL)
+	while ((newNode = head) != NULL)
 	{
 		head = head->next;
-		free(new->str);
-		free(new);
+		free(newNode->str);
+		free(newNode);
 	}
 }
