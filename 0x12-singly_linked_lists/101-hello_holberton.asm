0e93783fa7a517223dd1a827550413f617945ace
Ozioma7 <okonisaozioma@gmail.com>
2023-04-27 18:52:07 +0000

    Assembly

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,3 +1,7 @@
+section .data
+msg:	db "Hello, Holberton", 0
+fmt:	db "%s", 10 0
+
 section .text
 	extern printf
 	global main
@@ -5,12 +9,8 @@ section .text
 main:
 	mov esi, msg
 	mov edi, fmt
-	mov eax, 0	;system call (sys_write)
-	call printf	;to call kernel
+	mov eax, 0
+	call printf
 
-	mov eax, 0	;system call (sys_exit)
+	mov eax, 0
 	ret
-
-section .data
-msg	db "Hello, Holberton",0xa ; also 10
-ftm:	db "%s", 10, 0
