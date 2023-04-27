51a9576f82d31d458abb11cd370bfc5c3a1edcac
Ozioma7 <okonisaozioma@gmail.com>
2023-04-27 11:58:19 +0000

    lists.h

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -14,11 +14,11 @@
  * Description: Singly linked list
  */
 
-typedef struct node
+typedef struct list_s
 {
 	char *str;
 	unsigned int len;
-	struct node *next;
+	struct list_s *next;
 } list_t;
 
 size_t print_list(const list_t *h);
