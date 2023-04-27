8b8d39f6b255b2a38b90ce4a93d1d3dc036790ba
Ozioma7 <okonisaozioma@gmail.com>
2023-04-27 12:27:22 +0000

    task3

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -12,16 +12,16 @@ list_t *add_node_end(list_t **head, const char *str)
 {
 	list_t *newNode, *temp;
 	size_t Num;
-	
+
 	newNode = malloc(sizeof(list_t));
 	if (newNode == NULL)
 		return (NULL);
-	
+
 	newNode->str = strdup(str);
-	
+
 	for (Num = 0; str[Num]; Num++)
 		;
-	
+
 	newNode->len = Num;
 	newNode->next = NULL;
 	temp = *head;
