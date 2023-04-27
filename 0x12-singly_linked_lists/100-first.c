3c400f9bb09ba81fb5e8ec865645f8a3eb19417b
Ozioma7 <okonisaozioma@gmail.com>
2023-04-27 18:00:29 +0000

    task5

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -8,5 +8,5 @@
 void __attribute__ ((constructor)) beforeMain()
 {
 	printf("You're beat! and yet, you must allow, \nI");
-	printf("bore house upon my back!\n");
+	printf(" bore house upon my back!\n");
 }
