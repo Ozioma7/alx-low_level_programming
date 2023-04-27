13e64dd5482a3c8f16c21bcf78c45e563223559a
Ozioma7 <okonisaozioma@gmail.com>
2023-04-27 11:46:41 +0000

    Singly linked list

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,6 +1,4 @@
 #include "lists.h"
-#include <stdlib.h>
-#include <stdio.h>
 
 /**
  * print_list - Writes a function that prints all the elements of a list_t list
@@ -10,7 +8,7 @@
 
 size_t print_list(const list_t *h)
 {
-	size_t Num
+	size_t Num;
 
 	Num = 0;
 	while (h != NULL)
--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,6 +1,20 @@
 #ifndef LISTS_H
 #define LISTS_H
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+/**
+ * struct node - singly linked list
+ * @str: string
+ * @len: lenght of string
+ */
 
+typedef struct node
+{
+	char *str;
+	unsigned int len;
+	struct node *next;
+} list_t;
 
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
