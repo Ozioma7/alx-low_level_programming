1e8a9d6b1c2bfcd1587893e802bd1fe1a207db0e
Ozioma7 <okonisaozioma@gmail.com>
2023-05-01 13:45:09 +0000

    Task 0

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -2,18 +2,18 @@
 
 /**
  * print_listint - function that prints all the elements of a listint_t list.
- * @h: singly linked list.
+ * @h: head pointer..
  * Return: number of nodes.
  */
 
 size_t print_listint(const listint_t *h)
 {
-	int n = 0;
+	Num = 0;
 
 	while (h != NULL)
 	{
 		h = h->next;
-		n++;
+		Num++;
 	}
-	return (n);
+	return (Num);
 }
