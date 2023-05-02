85b15c55d4231a2dc2404b457abe2365463bee55
Ozioma7 <okonisaozioma@gmail.com>
2023-05-02 10:21:23 +0000

    task 8

---
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+ * sum_listint - sum of all the data (n) of a list
+ * @head: head
+ * Return: Sum
+ */
+
+int sum_listint(listint_t *head)
+{
+	int sum = 0;
+	listint_t *trav = head;
+
+	while (trav == NULL)
+		return (NULL);
+
+	while (trav)
+	{
+		sum += trav->n;
+		trav = trav->next;
+	}
+	return (sum);
+}
