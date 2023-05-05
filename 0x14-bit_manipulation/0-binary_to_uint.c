bdd9db7393a424f077fc4c1fe943513bae1c33be
Ozioma7 <okonisaozioma@gmail.com>
2023-05-05 18:13:31 +0000

    task zero

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * binary_to_uint - nverts a binary number to an unsigned int
+ * binary_to_uint - converts a binary number to an unsigned int
  * @b: String containing binary number
  * Return: Result or 0
  */
@@ -10,13 +10,13 @@ unsigned int binary_to_uint(const char *b)
 	int a = 0;
 
 	if (b == NULL)
-		return 0;
+		return (0);
 
-	while (b[a] == '0' || b[a] == '1')
+	for (a = 0; b[a]; a++)
 	{
-		con <<= 1;
-		con += b[a]-'0';
-		a++;
+		if (b[a] < '0' || b[a] > '1')
+			return (0);
+		con = 2 * con + (b[a] - '0');
 	}
 	return (con);
 }
