6242fb7380e113bab91466f45d783b89f46f51b2
Ozioma7 <okonisaozioma@gmail.com>
2023-05-27 20:50:43 +0000

    task three

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -2,22 +2,22 @@
 
 /**
  * fileError - If the file can open..
- * @home_file: file_from.
- * @destn_file: file_to.
+ * @file_from: file_from.
+ * @file_to: file_to.
  * @argv: arguments vector.
  *
  * Return: no return.
  */
-void fileError(int home_file, int destn_file, char *argv[])
+void fileError(int file_from, int file_to, char *argv[])
 {
 	if (home_file == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Error While Reading %s\n", argv[1]);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
-	if (destn_file == -1)
+	if (file_to == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Error why Writing %s\n", argv[2]);
+		dprintf(STDERR_FILENO, "Error: Can't write to file %s\n", argv[2]);
 		exit(99);
 	}
 }
@@ -31,42 +31,42 @@ void fileError(int home_file, int destn_file, char *argv[])
  */
 int main(int argc, char *argv[])
 {
-	int home_file, destn_file, err_close;
+	int file_from, file_to, err_close;
 	ssize_t num_chars, n;
 	char buf[1024];
 
 	if (argc != 3)
 	{
-		dprintf(STDERR_FILENO, "%s\n", "Usage: cp home_file destn_file");
+		dprintf(STDERR_FILENO, "%s\n", "Usage: cp file_from file_to");
 		exit(97);
 	}
 
-	home_file = open(argv[1], O_RDONLY);
-	destn_file = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC | O_APPEND, 0664);
-	error_file(home_file, destn_file, argv);
+	file_from = open(argv[1], O_RDONLY);
+	file_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC | O_APPEND, 0664);
+	error_file(file_from, file_to, argv);
 
 	num_chars = 1024;
 	while (num_chars == 1024)
 	{
-		num_chars = read(home_file, buf, 1024);
+		num_chars = read(file_from, buf, 1024);
 		if (num_chars == -1)
 			error_file(-1, 0, argv);
-		n = write(destn_file, buf, num_chars);
+		n = write(file_to, buf, num_chars);
 		if (nwr == -1)
 			error_file(0, -1, argv);
 	}
 
-	err_close = close(home_file);
+	err_close = close(file_from);
 	if (err_close == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", home_file);
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_from);
 		exit(100);
 	}
 
-	err_close = close(destn_file);
+	err_close = close(file_to);
 	if (err_close == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", home_file);
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_from);
 		exit(100);
 	}
 	return (0);
